d37d5860b2b32d6609ce0c1827e094f3b6e46f5f
Kevin <lennoz.kevin@hotmail.fr>
2020-11-24 17:40:58 +0100

    Récupération des internProfile en Liste

--- src/fr/eql/ai108/model/CreationAnnuaire.java
+++ src/fr/eql/ai108/model/CreationAnnuaire.java
@@ -3,68 +3,60 @@ package fr.eql.ai108.model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreationAnnuaire {
 
 	private static String originalFilePath = "./stagiaires.txt";
 	private static String internBDD = "./internBDD.bin";
 
-	private int maxSurnameSize = 0;
-	private int maxFirstNameSize = 0;
-	private int maxCountySize = 0;
-	private int maxPromSize = 0;
-	private int maxStudyYearSize = 0;
-	private int internDataSize = 0;
-
 	public CreationAnnuaire() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public void computeMaxSize() {
+	public List<InternProfile> extractInternDatas() {
 
 		BufferedReader br = null;
+		
+		List<InternProfile> internProfiles = new ArrayList<InternProfile>();
+		
 		try {
 			br = new BufferedReader(new FileReader(originalFilePath));
-			String line = br.readLine();
-
-			int i = 0;
-			while (line != null) {
-				if (!line.equals("*")) {
-						// i correspond au saut de ligne. En fonction de la valeur de i calcul la taille max de la variable.
+			String line;
+			
+			while ((line = br.readLine()) != null) { 
+				
+				InternProfile internProfile = new InternProfile();
+				
+				for (int i = 0; i < 5; i++) {
+					
 					switch (i) {
 					case 0:
-						if (line.length() > maxSurnameSize) {
-							maxSurnameSize = line.length();
-						}
+						internProfile.setSurname(line.trim());
 						break;
 					case 1:
-						if (line.length() > maxFirstNameSize) {
-							maxFirstNameSize = line.length();
-						}
+						internProfile.setFirstName(line.trim());
 						break;
 					case 2:
-						if (line.length() > maxCountySize) {
-							maxCountySize = line.length();
-						}
+						internProfile.setCounty(line.trim());
 						break;
 					case 3:
-						if (line.length() > maxPromSize) {
-							maxPromSize = line.length();
-						}
+						internProfile.setPromotion(line.trim());
 						break;
 					case 4:
-						if (line.length() > maxStudyYearSize) {
-							maxStudyYearSize = line.length();
-						}
+						internProfile.setStudyYear(Integer.parseInt(line.trim()));
+						break;
+					default:
 						break;
 					}
-					i++;
-				} else {
-					i = 0;
+					
+					line = br.readLine();
 				}
+				
+				internProfiles.add(internProfile);
+				
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -74,12 +66,6 @@ public class CreationAnnuaire {
 				e.printStackTrace();
 			}
 		}
-		internDataSize = maxSurnameSize + maxFirstNameSize + maxCountySize + maxPromSize + maxStudyYearSize;
+		return internProfiles;
 	}
-
-
-
-
-
-
 }
---
+++ src/fr/eql/ai108/model/InternProfile.java
@@ -0,0 +1,107 @@
+package fr.eql.ai108.model;
+
+public class InternProfile {
+	
+	private String surname;
+	private String firstName;
+	private String county;
+	private String promotion;
+	private int studyYear;
+	
+	
+	public InternProfile() {
+		super();
+	}
+
+	public InternProfile(String surname, String firstName, String county, String promotion, int studyYear) {
+		super();
+		this.surname = surname;
+		this.firstName = firstName;
+		this.county = county;
+		this.promotion = promotion;
+		this.studyYear = studyYear;
+	}
+
+	@Override
+	public String toString() {
+		return "InternProfile [surname=" + surname + ", firstName=" + firstName + ", county=" + county + ", promotion="
+				+ promotion + ", studyYear=" + studyYear + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((county == null) ? 0 : county.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((promotion == null) ? 0 : promotion.hashCode());
+		result = prime * result + studyYear;
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InternProfile other = (InternProfile) obj;
+		if (county == null) {
+			if (other.county != null)
+				return false;
+		} else if (!county.equals(other.county))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (promotion == null) {
+			if (other.promotion != null)
+				return false;
+		} else if (!promotion.equals(other.promotion))
+			return false;
+		if (studyYear != other.studyYear)
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public String getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
+	public int getStudyYear() {
+		return studyYear;
+	}
+	public void setStudyYear(int studyYear) {
+		this.studyYear = studyYear;
+	}
+}
