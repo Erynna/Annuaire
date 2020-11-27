238802386fefe209b22f6586ee63cf24f25cde37
Kevin <lennoz.kevin@hotmail.fr>
2020-11-27 18:00:22 +0100

    modification methode addAdmin

--- src/fr/eql/ai108/model/AdminUserDao.java
+++ src/fr/eql/ai108/model/AdminUserDao.java
@@ -1,6 +1,8 @@
 package fr.eql.ai108.model;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -11,16 +13,23 @@ public class AdminUserDao {
 	public void addAdmin(AdminUser adminUser) {
 		
 		File file = new File(loginBDDPath);
+		FileWriter out = null;
 		RandomAccessFile raf = null;
-
 		
 		try {
-			
-			raf = new RandomAccessFile(file, "rw");
-			
+					
 			file.createNewFile();
 			
-			raf.writeBytes(adminUser.getLogin() + "_" + adminUser.getPassword());
+			out = new FileWriter(file, true);
+			raf = new RandomAccessFile(file, "rw");
+			
+			raf.writeBytes(adminUser.getLogin());
+			raf.writeByte(0);
+			raf.writeBytes(adminUser.getPassword() + "\r");
+			
+			raf.writeBytes("Bob0");
+			raf.writeByte(0);
+			raf.writeBytes("cool" + "\r");
 			
 		} catch (IOException e) {
 			e.printStackTrace();
