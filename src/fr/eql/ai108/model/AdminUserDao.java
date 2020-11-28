ed24c68e19e1f6fd481f2acb8122b6c52a75a704
Kevin <lennoz.kevin@hotmail.fr>
2020-11-28 21:16:58 +0100

    Creation methodes : addAdmin, checkLoginExistence et collectPassword

--- src/fr/eql/ai108/model/AdminUserDao.java
+++ src/fr/eql/ai108/model/AdminUserDao.java
@@ -1,72 +1,61 @@
 package fr.eql.ai108.model;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
 
 public class AdminUserDao {
 
 	private String loginBDDPath = "./login.bin";
+	private File loginsBDD = new File(loginBDDPath);
+	private long pointerPosition = 0;						//Variable de classe nécessaire afin de stocker l'information de positionnement du RandomAccessFile renvoyé par checkLoginExistence et utilisée dans collectPassword
 
+	/*
+	 * Méthode qui permet l'ajout d'un nouvel "Administrateur" possédant un identifiant et un mot de passe personnalisé
+	 */
 	public void addAdminAccount(String login, String password) {
 
-		File file = new File(loginBDDPath);
 		FileWriter out = null;
 		BufferedWriter bw = null;
+		RandomAccessFile raf = null;
 
 		try {
 
-			file.createNewFile();
+			raf = new RandomAccessFile(loginsBDD, "rw");
 
-			out = new FileWriter(file, true);
+			loginsBDD.createNewFile();
+
+			out = new FileWriter(loginsBDD, true);
 			bw = new BufferedWriter(out);
 
-			bw.write(login);
-			bw.write(0);											//Ecriture d'un octet null afin de séparer les champs Login et Password
-			bw.write(password + "\r");
+			if(!checkLoginExistence(login)) {
+				bw.write(login);
+				bw.write(0);											//Ecriture d'un octet null afin de séparer les champs Login et Password
+				bw.write(password + "\r");
+			}
+
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}finally {
 			try {
 				bw.close();
+				out.close();
+				raf.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	//	public void modifyPassword(String initialLogin, String initialPassword, String newPassword) {
-	//
-	//		File file = new File(loginBDDPath);
-	//		RandomAccessFile raf = null;
-	//		int sizeLogin = 0;
-	//		byte[] initialLoginInBytes = initialLogin.getBytes();
-	//		byte[] ligne = new byte[20];
-	//
-	//		try {
-	//			raf = new RandomAccessFile(file, "rw");
-	//		
-	//			
-	//			
-	//		} catch (IOException e) {
-	//			e.printStackTrace();
-	//		}
-	//	}
+	/*
+	 * Méthode qui renvoie le mot de passe administrateur dans le cas où l'identifiant existe bien dans la BDD, et qui retourne "" dans le cas contraire
+	 */
+	public String collectPassword(String login) {
 
-	public void collectPassword(String login) {
-
-		File loginsBDD = new File(loginBDDPath);
-		byte actualReadByte;
-		byte[] loginFromBDD;
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		String convertedLogin;
 		String password = "";
 		RandomAccessFile raf = null;
 
@@ -74,46 +63,97 @@ public class AdminUserDao {
 
 			raf = new RandomAccessFile(loginsBDD, "rw");
 
-			do {
-				while ((actualReadByte = raf.readByte()) != 0) {			
+			if (checkLoginExistence(login)) {
 
-					outputStream.write(actualReadByte);
+				raf.seek(pointerPosition);
+				password = raf.readLine();
 
-				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				raf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return password;
+	}
 
-				loginFromBDD = outputStream.toByteArray();
-				convertedLogin = new String(loginFromBDD);
+	/*
+	 * Méthode qui renvoie "true" si le login existe dans la BDD et "false" dans le cas contraire
+	 */
+	public boolean checkLoginExistence(String login) {
 
-				if(convertedLogin.equals(login)) {
+		boolean loginExistence = false;
+		byte actualReadByte;
+		byte[] loginFromBDD;
+		RandomAccessFile raf = null;
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		String convertedLogin = "";
+		int fileLength = 0;
 
-					password = raf.readLine();
+		try {
 
-				}
+			raf = new RandomAccessFile(loginsBDD, "rw");
+			fileLength = getFileLength(raf);
 
-				System.out.println(password);
+			if (fileLength > 0) {
+				do {
 
-			//On Balaye chaque ligne 
-			//si le premier caractere est similaire alors 
-			//on compare les bytes avec les caracteres:
-			//On recupere chaque byte que l'on caste en char
-			//On compare les deux ensuite
-			//Si equivalence on renvoi le password
+					while ((actualReadByte = raf.readByte()) != 0) {			//Lecture de l'identifiant courant jusqu'au séparateur : octet nul
 
+						outputStream.write(actualReadByte);						//Ajout de chaque octet de données à l'OutputStream afin de recomposer l'identifiant
 
-			//Sinon on continue de balayer les lignes
-			//Fin si rien trouvé
-			//Le compte n'existe pas
+					}
 
+					loginFromBDD = outputStream.toByteArray();
+					outputStream.reset();
+					convertedLogin = new String(loginFromBDD);
 
+					if(convertedLogin.equals(login)) {
+						pointerPosition = raf.getFilePointer();
+						loginExistence = true;
+						break;
+					}else {
+						raf.readLine();
+					}
 
+				}while(raf.getFilePointer() < fileLength);						//Bouclage sur la totalité du fichier
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				outputStream.close();
+				raf.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
+		return loginExistence;
+	}
 
+	/*
+	 * Méthode qui permet de récupérer la taille des données àl'intérieur de notre fichier
+	 */
+	private int getFileLength(RandomAccessFile raf) {
+
+		int position = 0;
+
+		try {
+			while(raf.readLine() != null) {
+
+				position = (int) raf.getFilePointer();
+
+			}
+			raf.seek(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		return position;
 	}
-
-
-
-
 }
