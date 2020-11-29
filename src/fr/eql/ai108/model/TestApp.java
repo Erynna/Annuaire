d8c97cabc9a07e6674b7075b6db919e6bf9dfb3e
Benoit <benoitmarejus@hotmail.fr>
2020-11-29 21:19:13 +0100

    InternProfileDao avec méthode supprimer

--- src/fr/eql/ai108/model/TestApp.java
+++ src/fr/eql/ai108/model/TestApp.java
@@ -6,6 +6,11 @@ public class TestApp {
 
 	public static void main(String[] args) {
 		
+		InternProfileDao ipd = new InternProfileDao("C:\\Users\\formation\\Desktop\\internBDD.bin");
+		//ipd.deleteInternProfile("LEPANTE", "Willy", "95", "AI 78", 2010);
+		for (InternProfile ip : ipd.getAll()) {
+			System.out.print(ip);
+		}
 	}
 
 }
