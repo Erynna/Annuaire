07c225e6c38a308a278ac1e7eec801a5e9040fb0
Kevin <lennoz.kevin@hotmail.fr>
2020-11-29 19:25:48 +0100

    Modification récupération données fichiers optimisé

--- src/fr/eql/ai108/model/InternProfileDao.java
+++ src/fr/eql/ai108/model/InternProfileDao.java
@@ -12,18 +12,6 @@ import java.util.List;
 public class InternProfileDao {
 
 	private String internBDD;
-	private final int byteForLeftChild = 0;   			// désigne l'emplacement du 1er byte où est inscrite la position de l'enfant gauche
-	private final int byteForRightChild = 4;  			// désigne l'emplacement du 1er byte où est inscrite la position de l'enfant droit
-	private final int byteForSurname = 8;     			// désigne l'emplacement du 1er byte où est inscrit le nom du stagiaire
-	private final int numberOfBytesForSurname = 21;		// désigne le nombre de bytes réservés pour inscrire le nom de famille
-	private final int byteForFirstName = 29;			// désigne l'emplacement du 1er byte où est inscrit le prénom du stagiaire
-	private final int numberOfBytesForFirstName = 20;	// désigne le nombre de bytes réservés pour inscrire le prénom
-	private final int byteForCounty = 49;				// désigne l'emplacement du 1er byte où est inscrit le département du stagiaire
-	private final int numberOfBytesForCounty = 2;		// désigne le nombre de bytes réservés pour inscrire le département du stagiaire
-	private final int byteForPromotion = 51;			// désigne l'emplacement du 1er byte où est inscrit le nom de la promotion
-	private final int numberOfBytesForPromotion = 10;	// désigne le nombre de bytes réservés pour inscrire la promotion
-	private final int byteForStudyYear = 61;			// désigne l'emplacement du 1er byte où est inscrite l'année d'étude
-	private final int numberOfBytesForStudyYear = 4;	// désigne le nombre de bytes réservés pour inscrire l'année d'étude
 	
 	/*
 	 * Pour créer une instance de cette classe, il faut passer en argument dans le constructeur le nom de l'annuaire
@@ -41,44 +29,20 @@ public class InternProfileDao {
 	 * @ return : un objet InternProfile constitué à partir des informations lues dans le fichier binaire
 	 */
 	private InternProfile extractDatasIntern(RandomAccessFile raf, int cursorPosition) {
+		
 		InternProfile internProfile = null;
+		int datasStartPosition = 8;				//Démarrage des données pour chaque stagiaire après 2 x 4 octets (positions des enfants)
+		
 		try {
-			//récupère le nom de famille
-			raf.seek(cursorPosition+byteForSurname);  
-			byte[] bytesSurname = new byte[numberOfBytesForSurname];
-			for (int i=0;i<numberOfBytesForSurname;i++) {
-				bytesSurname[i]=raf.readByte();
-			}
-			String surname = new String(bytesSurname);
-			//récupère le prénom
-			raf.seek(cursorPosition+byteForFirstName);
-			byte[] bytesFirstName = new byte[numberOfBytesForFirstName];
-			for (int i=0;i<numberOfBytesForFirstName;i++) {
-				bytesFirstName[i]=raf.readByte();
-			}
-			String firstName = new String(bytesFirstName);
-			//récupère le département
-			raf.seek(cursorPosition+byteForCounty);
-			byte[] bytesCounty = new byte[numberOfBytesForCounty];
-			for (int i=0;i<numberOfBytesForCounty;i++) {
-				bytesCounty[i]=raf.readByte();
-			}
-			String county = new String(bytesCounty);
-			//récupère la promotion
-			raf.seek(cursorPosition+byteForPromotion);
-			byte[] bytesPromotion = new byte[numberOfBytesForPromotion];
-			for (int i=0;i<numberOfBytesForPromotion;i++) {
-				bytesPromotion[i]=raf.readByte();
-			}
-			String promotion = new String(bytesPromotion);
-			//récupère l'année d'étude
-			raf.seek(cursorPosition+byteForStudyYear);
-			byte[] bytesStudyYear = new byte[numberOfBytesForStudyYear];
-			for (int i=0;i<numberOfBytesForStudyYear;i++) {
-				bytesStudyYear[i]=raf.readByte();
-			}
-			String studyYear = new String(bytesStudyYear);
-			internProfile = new InternProfile(surname.trim(), firstName.trim(), county.trim(), promotion.trim(), Integer.parseInt(studyYear));
+			
+			raf.seek(cursorPosition + datasStartPosition);
+			String surname = raf.readUTF().trim();
+			String firstName = raf.readUTF().trim();
+			String county = raf.readUTF().trim();
+			String promotion = raf.readUTF().trim();
+			int studyYear = raf.readInt();
+			
+			internProfile = new InternProfile(surname, firstName, county, promotion, studyYear);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -95,7 +59,7 @@ public class InternProfileDao {
 	private int positionLeftChild(RandomAccessFile raf, int cursorPosition) {
 		int positionToCatch=0;
 		try {
-			raf.seek(cursorPosition+byteForLeftChild);
+			raf.seek(cursorPosition);
 			positionToCatch = raf.readInt();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -112,6 +76,8 @@ public class InternProfileDao {
 	 */
 	private int positionRightChild(RandomAccessFile raf, int cursorPosition) {
 		int positionToCatch=0;
+		int byteForRightChild = 4;
+		
 		try {
 			raf.seek(cursorPosition+byteForRightChild);
 			positionToCatch = raf.readInt();
