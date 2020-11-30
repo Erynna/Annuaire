4560180776abc6d47fae4d16cd82b58f7aab70e0
Kevin <lennoz.kevin@hotmail.fr>
2020-11-30 10:33:28 +0100

    Retour en arrière (formatage fichier optimisé)

--- src/fr/eql/ai108/model/CreationAnnuaire.java
+++ src/fr/eql/ai108/model/CreationAnnuaire.java
@@ -1,6 +1,7 @@
 package fr.eql.ai108.model;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,6 +15,11 @@ public class CreationAnnuaire {
 
 	private final String originalFilePath = "./stagiaires.txt";
 	private final String internBDDPath = "./internBDD.bin";
+	private int maxSurnameLength = 0;
+	private int maxFirstNameLength = 0;
+	private int maxCountyLength = 0;
+	private int maxPromotionLength = 0;
+	private int maxYearStudyLength = 4;
 
 	public CreationAnnuaire() {
 		super();
@@ -49,15 +55,27 @@ public class CreationAnnuaire {
 					switch (i) {
 					case 0:
 						surname = line.trim();
+						if (surname.length() > maxSurnameLength) {
+							maxSurnameLength = surname.length();
+						}
 						break;
 					case 1:
 						firstName = line.trim();
+						if (firstName.length() > maxFirstNameLength) {
+							maxFirstNameLength = firstName.length();
+						}
 						break;
 					case 2:
 						county = line.trim();
+						if (county.length() > maxCountyLength) {
+							maxCountyLength = county.length();
+						}
 						break;
 					case 3:
 						promotion = line.trim();
+						if (promotion.length() > maxPromotionLength) {
+							maxPromotionLength = promotion.length();
+						}
 						break;
 					case 4:
 						studyYear = Integer.parseInt(line.trim());
@@ -147,14 +165,83 @@ public class CreationAnnuaire {
 	}
 
 	/*
+	 * Méthode qui permet à partir d'un tableau de caractere de générer un tableau ordonné de bytes et de compléter par des bytes vides pour s'adapter aux tailles max des données
+	 */
+	public byte[] convertCharToByte(char[] data, int dataMaxLength) {
+
+		byte[] dataInByte = new byte[dataMaxLength];
+
+		for (int i = 0; i < data.length; i++) {
+
+			dataInByte[i] = (byte) data[i];
+		}
+
+		if (data.length < dataMaxLength) {
+
+			for (int i = data.length; i < dataMaxLength; i++) {
+
+				dataInByte[i] = 0;
+
+			}
+		}
+
+		return dataInByte;
+	}
+
+	/*
+	 * Méthode qui permet de concatener l'ensemble des donnees presentes dans les differents tableaux de bytes sous la forme d'un seul et unique tableau de byte
+	 */
+	public byte[] concatenateInternDatas(InternProfile internProfile) {
+
+		ByteArrayOutputStream outputStream;
+		byte[] fullDatas;
+		
+		char[] surname = internProfile.getSurname().toCharArray();
+		char[] firstName = internProfile.getFirstName().toCharArray();
+		char[] county = internProfile.getCounty().toCharArray();
+		char[] promotion = internProfile.getPromotion().toCharArray();
+		char[] yearStudy = String.valueOf(internProfile.getStudyYear()).toCharArray();
+
+		byte[] surnameByte = convertCharToByte(surname, maxSurnameLength);
+		byte[] firstNameByte = convertCharToByte(firstName, maxFirstNameLength);
+		byte[] countyByte = convertCharToByte(county, maxCountyLength);
+		byte[] promotionByte = convertCharToByte(promotion, maxPromotionLength);
+		byte[] yearStudyByte = convertCharToByte(yearStudy, maxYearStudyLength);
+
+		outputStream = new ByteArrayOutputStream(); 
+		
+		try {
+			outputStream.write(surnameByte);
+			outputStream.write(firstNameByte); 
+			outputStream.write(countyByte); 
+			outputStream.write(promotionByte); 
+			outputStream.write(yearStudyByte); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				outputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		fullDatas = outputStream.toByteArray(); 
+
+		return fullDatas;
+	}
+
+	/*
 	 * Méthode qui permet récursivement d'écrire les données stagiaires et les positions des enfants sous la forme d'une arborescence binaire en octets.
 	 */
 	private void writeChildrenInterns(InternProfile internProfile, RandomAccessFile raf) {
 
 		InternProfile leftChild = internProfile.getLeftChild();
 		InternProfile rightChild = internProfile.getRightChild();
-		int offsetLeftPosition = internProfile.getSizeOfCurrentInternDatas();				//Offset lié à l'écriture des 4 octets de la position enfant gauche, des 4 octets de la position enfant droit, de l'ensemble des données stagiaire et du separateur final
-		int offsetRightPosition = internProfile.getLeftChild().getSizeOfChildrenDatas();				//Offset lié à l'écriture des 4 octets de la position enfant droit, de l'ensemble des données stagiaire et du separateur final
+		int fullDatasLength = maxSurnameLength + maxFirstNameLength + maxCountyLength + maxPromotionLength + maxYearStudyLength;
+		int offsetLeftPosition = fullDatasLength + 9;				//Offset lié à l'écriture des 4 octets de la position enfant gauche, des 4 octets de la position enfant droit, de l'ensemble des données stagiaire et du separateur final
+		int offsetRightPosition = fullDatasLength + 5;				//Offset lié à l'écriture des 4 octets de la position enfant droit, de l'ensemble des données stagiaire et du separateur final
+		byte[] tab = concatenateInternDatas(internProfile);
 
 		try {
 
@@ -170,7 +257,7 @@ public class CreationAnnuaire {
 
 			if(!rightChild.isEmpty) {										//Si présence d'un element droit on indique sa position
 
-				raf.writeInt((int)raf.getFilePointer() + (offsetLeftPosition - 4) + offsetRightPosition); //On indique que l'enfant droit se situe à la position du pointeur + l'offSetLeftPosition - 4 octets de la position gauche + offSetRightPosition (longueur des données stagiaire de l'arborescence gauche)
+				raf.writeInt((int)raf.getFilePointer() + offsetRightPosition + (leftChild.getNumberOfChildren() * offsetLeftPosition)); //On indique que l'enfant droit se situe à la position du pointeur l'offSetRightPosition (longueur des données stagiaire plus 4 octets de données + 1 octet separateur) + l'ensemble des données des enfants gauches
 
 			}else if (rightChild.isEmpty) {
 
@@ -178,11 +265,8 @@ public class CreationAnnuaire {
 
 			}
 
-			raf.writeUTF(internProfile.getSurname());						//Traitement initial : Ecriture des données de l'élément courant		
-			raf.writeUTF(internProfile.getFirstName());
-			raf.writeUTF(internProfile.getCounty());
-			raf.writeUTF(internProfile.getPromotion());
-			raf.writeInt(internProfile.getStudyYear());
+			raf.write(tab);													//Traitement initial : Ecriture des données de l'élément courant		
+
 			raf.writeBytes("\r");
 
 			if(!leftChild.isEmpty){											//Si element possède un enfant gauche : Recursivité à Gauche
@@ -200,5 +284,45 @@ public class CreationAnnuaire {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public int getMaxSurnameLength() {
+		return maxSurnameLength;
+	}
+
+	public void setMaxSurnameLength(int maxSurnameLength) {
+		this.maxSurnameLength = maxSurnameLength;
+	}
+
+	public int getMaxFirstNameLength() {
+		return maxFirstNameLength;
+	}
+
+	public void setMaxFirstNameLength(int maxFirstNameLength) {
+		this.maxFirstNameLength = maxFirstNameLength;
+	}
+
+	public int getMaxCountyLength() {
+		return maxCountyLength;
+	}
+
+	public void setMaxCountyLength(int maxCountyLength) {
+		this.maxCountyLength = maxCountyLength;
+	}
+
+	public int getMaxPromotionLength() {
+		return maxPromotionLength;
+	}
+
+	public void setMaxPromotionLength(int maxPromotionLength) {
+		this.maxPromotionLength = maxPromotionLength;
+	}
+
+	public int getMaxYearStudyLength() {
+		return maxYearStudyLength;
+	}
+
+	public void setMaxYearStudyLength(int maxYearStudyLength) {
+		this.maxYearStudyLength = maxYearStudyLength;
 	}
 }
